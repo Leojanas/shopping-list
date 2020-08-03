670aec18b8575df3ae651dadd3ce34819ac1bbbc
Leo Janas <64052101+Leojanas@users.noreply.github.com>
2020-08-03 15:29:24 -0600

    Update index.js
    Added comments to index.js file.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//This is the js file for the shopping list app
+//This function allows the user to add items to the list
 function addItem() {
     $('#js-shopping-list-form').submit(function(event) {
         event.preventDefault();
@@ -13,17 +15,21 @@ function addItem() {
           '</button>' +
         '</div>' +
       '</li>')
+        //This line clears the field after it is submitted so that a new item can be typed
       $(this).find('#shopping-list-entry').val('');
     }
     )
 }
+//This function uses the 'check' button to toggle the strikethrough for the items
 function checkToggle() {
+    //Both the checkToggle and deleteItem functions use event delegation to apply to new items as well
   $('ul').on('click', '.shopping-item-toggle', function(event) {
     event.preventDefault;
     $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked') 
   }
   )
 }
+//This function uses the delete button to remove items from the list permanently
 function deleteItem() {
   $('ul').on('click', '.shopping-item-delete', function(event) {
     event.preventDefault;
@@ -31,6 +37,7 @@ function deleteItem() {
   }
   )
 }
+//This code runs all three functions when the page is loaded
 $(addItem);
 $(deleteItem);
 $(checkToggle);
