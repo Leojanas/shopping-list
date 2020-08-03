2bf197323c2248b5fe8646f7479fbe750a3e2c9e
Leo Janas <leojanas7@gmail.com>
2020-08-03 15:09:13 -0600

    Added remaining features and debugged.

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 function addItem() {
-    $('.js-shopping-list-form').submit(function(event) {
+    $('#js-shopping-list-form').submit(function(event) {
         event.preventDefault();
-        let newItem = $(this).find('#shopping-list-entry').text()
-        let newItemHTML = <li>
-        <span class="shopping-item">" + newItem + "</span>
-        <div class="shopping-item-controls">
-          <button class="shopping-item-toggle">
-            <span class="button-label">check</span>
-          </button>
-          <button class="shopping-item-delete">
-            <span class="button-label">delete</span>
-          </button>
-        </div>
-      </li>;
-        $('.shopping-list').append(newItemHTML);
+        let newItem = $(this).find('#shopping-list-entry').val();
+        $('ul').append('<li>' + 
+        '<span class="shopping-item">' + newItem + '</span>' +
+        '<div class="shopping-item-controls">' +
+          '<button class="shopping-item-toggle">' +
+            '<span class="button-label">check</span>' +
+          '</button>' +
+          '<button class="shopping-item-delete">' +
+            '<span class="button-label">delete</span>' +
+          '</button>' +
+        '</div>' +
+      '</li>')
     }
     )
 }
+function checkToggle() {
+  $('ul').on('click', '.shopping-item-toggle', function(event) {
+    event.preventDefault;
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked') 
+  }
+  )
+}
+function deleteItem() {
+  $('ul').on('click', '.shopping-item-delete', function(event) {
+    event.preventDefault;
+    $(this).closest('li').remove();
+  }
+  )
+}
+$(addItem);
+$(deleteItem);
+$(checkToggle);
+
